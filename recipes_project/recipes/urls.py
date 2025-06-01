054c9f3615d9c8f903e817f646d826ff84477f4b
Artyom Karpenko <azkarpenko@cloud.ru>
2025-06-01 22:07:22 +0300

    Добавлено несколько рецептов и поправлено отображение

--- recipes_project/recipes/urls.py
+++ recipes_project/recipes/urls.py
@@ -1,9 +1,14 @@
-from django.contrib import admin
-from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
+from django.urls import path
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('recipes.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('', views.index, name='index'),
+    path('recipe/<int:pk>/', views.recipe_detail, name='recipe_detail'),
+
+    path('register/', views.register_view, name='register'),
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
+
+    path('recipe/create/', views.recipe_create, name='recipe_create'),
+    path('recipe/<int:pk>/edit/', views.recipe_edit, name='recipe_edit'),
+]
